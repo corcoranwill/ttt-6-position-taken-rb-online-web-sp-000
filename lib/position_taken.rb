2045dae1ab4cba352da288778ac1fc78fee66c86
Will Corcoran <corcoranwill@gmail.com>
2020-02-03 22:06:11 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,15 @@
 # code your #position_taken? method here!
+
+# player passes in board position 2
+# conver to array index 1
+# send to position_taken? method
+  # check and see if position is:
+    # vacant == false
+    # contains an X == true
+    # contains an O == true
+
+
+# Validations
+
+# 1. Define a method that checks if a given space on the board is empty == " "
+# 2. Address "edge cases" - i.e. do you want to consider "" and empty space?
